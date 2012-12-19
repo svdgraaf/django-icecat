e99242785bed7a8ce01d1971669758fc803cf1b9
Sander van de Graaf <mail@svdgraaf.nl>
2012-12-19 10:25:55 +0100

    added placeholder for specs

--- icecat/models.py
+++ icecat/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import requests
+import hashlib
+from icecat import settings
 
 
 class Product(models.Model):
@@ -10,6 +13,36 @@ class Product(models.Model):
     on_market = models.BooleanField(default=False)
     supplier = models.ForeignKey('Supplier')
     category = models.ForeignKey('Category')
+
+    def __unicode__(self):
+        return u'{supplier} {name}'.format(supplier=self.supplier.name, name=self.name)
+
+    def get_specifications(self):
+        # check if the file is already present
+        xml = self._get_raw_source()
+        return xml
+
+    def _get_raw_source(self):
+        import os
+        url = 'https://data.icecat.biz/xml_s3/xml_server3.cgi?prod_id={prod_id};vendor={vendor};lang={lang};output={output}'.format(prod_id=self.part, vendor=self.supplier.name, output='productxml', lang='EN')
+
+        # check if filename is already downloaded
+        filename = 'icecat-' + hashlib.md5(url).hexdigest() + '.xml'
+        path = os.path.join(settings.TMP_PATH, filename)
+
+        if not os.path.isfile(path):
+            # File isn't present yet, let's download it
+            r = requests.get(url, auth=(settings.API_USERNAME, settings.API_PASSWORD))
+            f = open(path, 'wb')
+            f.write(r.text)
+            f.close()
+
+            return r.text
+        else:
+            f = open(path, 'r')
+            xml = f.read()
+            f.close()
+            return xml
 
 
 class Supplier(models.Model):
